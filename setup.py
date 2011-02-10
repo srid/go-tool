c356c93977fd54aeea1a5b770dfb48207759c90a
Sridhar Ratnakumar <sridharr@activestate.com>
2011-02-10 11:27:44 -0800

    revamp setup.py using setuptools

--- setup.py
+++ setup.py
@@ -2,17 +2,9 @@
 # Copyright (c) 2002-2008 ActiveState Software
 # Author: Trent Mick (trentm@gmail.com)
 
-"""Quick directory changing (super-cd)
-
-'go' is a simple command line script to simplify jumping between
-directories in the shell. You can create shortcut names for commonly
-used directories and invoke 'go <shortcut>' to switch to that directory
--- among other little features.
-"""
-
 import os
 import sys
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 try:
@@ -30,28 +22,20 @@ Programming Language :: Python :: 2
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-if sys.version_info < (2, 3):
-    # Distutils before Python 2.3 doesn't accept classifiers.
-    _setup = setup
-    def setup(**kwargs):
-        if kwargs.has_key("classifiers"):
-            del kwargs["classifiers"]
-        _setup(**kwargs)
-
-doclines = __doc__.split("\n")
+_top_dir = os.path.dirname(os.path.abspath(__file__))
+README = open(os.path.join(_top_dir, 'README.rst')).read()
 
 setup(
     name="go",
     version=go.__version__,
+    description='Quick directory changing (super-cd)',
+    long_description=README ,
     maintainer="Trent Mick",
     maintainer_email="trentm@gmail.com",
     url="http://code.google.com/p/go-tool/",
     license="http://www.opensource.org/licenses/mit-license.php",
-    platforms=["any"],
     py_modules=["go"],
     package_dir={"": "lib"},
-    description=doclines[0],
     classifiers=filter(None, classifiers.split("\n")),
-    long_description="\n".join(doclines[2:]),
 )
 
